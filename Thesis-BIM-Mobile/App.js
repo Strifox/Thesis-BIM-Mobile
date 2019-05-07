fdd2b897ddbebab9a144cedcc1258ee09715d115
André Topalovic Törnström <tornstromandre@gmail.com>
2019-05-07 11:30:00 +0200

    Added some UI, some node_modules, and permissions

--- Thesis-BIM-Mobile/App.js
+++ Thesis-BIM-Mobile/App.js
@@ -1,7 +1,23 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import ImagePicker from 'react-native-image-picker';
+import { Permissions } from 'expo';
 
 export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    state = {
+      image: null
+    }
+  }
+
+  takePicture = async () => {
+    await Permissions.askAsync(Permissions.CAMERA);
+    const { cancelled, uri } = await ImagePicker.launchCameraAsync({ allowsEditiing: false });
+    this.setState({ image: uri });
+  }
   render() {
     return (
       <View style={styles.container}>
@@ -13,9 +29,13 @@ export default class App extends React.Component {
           <Text>OCR:</Text>
           <TextInput style={styles.textInput}></TextInput>
           <Text>Amount:</Text>
-          <TextInput style={styles.textInput}></TextInput>
+          <TextInput style={styles.textInput} onChangeText={() => {}}></TextInput>
           <Text>Paydate:</Text>
           <TextInput style={styles.textInput}></TextInput>
+          <View style={{flexDirection: 'row'}}>
+              <TouchableOpacity style={[styles.button, {marginRight: 5}]} onPress={() => {this.takePicture()}}><View style={{flexDirection: 'row'}}><Icon name='camera' size={20} color='white'/><Text style={styles.buttonText}> Scan</Text></View></TouchableOpacity>
+              <TouchableOpacity style={[styles.button, {marginLeft: 5}]} onPress={() => {}}><View style={{flexDirection: 'row'}}><Icon name='save' size={22} color='white' /><Text style={styles.buttonText}> Save</Text></View></TouchableOpacity>
+          </View>
         </View>
       </View>
     );
@@ -34,9 +54,23 @@ const styles = StyleSheet.create({
     borderColor: '#000',
     borderRadius: 7,
     height: 30,
+    paddingLeft: 5,
   },
   form: {
-    width: 60 + '%',
+    width: 80 + '%',
     textAlign: 'left',
+  },
+  button: {
+    flex: 1,
+    borderRadius: 7,
+    marginTop: 10,
+    backgroundColor: 'black',
+    height: 30,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
   }
 });
