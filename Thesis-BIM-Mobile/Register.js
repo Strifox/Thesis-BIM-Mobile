2b8f69ad17a3e3521c0f3f6b9168f69d27766b9b
André Topalovic Törnström <tornstromandre@gmail.com>
2019-05-27 09:50:04 +0200

    Made the register function take user inputs

--- Thesis-BIM-Mobile/Register.js
+++ Thesis-BIM-Mobile/Register.js
@@ -4,29 +4,31 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 import Style from './Style';
 
 export default class Register extends React.Component {
-  
-  //Fixa så att data från textinputsen skickar i register
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      username: '',
+      password: '',
+      confirmedPassword: ''
+    };
+  }
 
   Register() {
-    let data = {
-      Username: 'Andre',
-      Email: 'andre@andre.com',
-      Password: 'Andre123',
-      PushToken: 0
+    if(this.state.password === this.state.confirmedPassword) {
+      fetch('https://thesis-bim-backend.azurewebsites.net/api/User/Register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'Application/JSON'
+        },
+        body: JSON.stringify({
+          userName: this.state.username,
+          email: this.state.email,
+          password: this.state.password,
+          expoPushToken: '0'
+        })
+      });
     }
-
-    fetch('https://thesis-bim-backend.azurewebsites.net/api/User/Register', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'Application/JSON'
-      },
-      body: JSON.stringify({
-        userName: 'Andre',
-        email: 'andre@andre.com',
-        password: 'Andre123',
-        expoPushToken: '0'
-      })
-    });
   }
 
   render() {
@@ -34,13 +36,13 @@ export default class Register extends React.Component {
         <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
             <View style={Style.form}>
                 <Text>Email:</Text>
-                <TextInput style={Style.textInput}></TextInput>
+                <TextInput style={Style.textInput} onChangeText={(text) => this.setState({ email: text })}></TextInput>
                 <Text>Username:</Text>
-                <TextInput style={Style.textInput}></TextInput>
+                <TextInput style={Style.textInput} onChangeText={(text) => this.setState({ username: text })}></TextInput>
                 <Text>Password:</Text>
-                <TextInput secureTextEntry={true} style={Style.textInput}></TextInput>
+                <TextInput secureTextEntry={true} style={Style.textInput} onChangeText={(text) => this.setState({ password: text })}></TextInput>
                 <Text>Confirm Password:</Text>
-                <TextInput secureTextEntry={true} style={Style.textInput}></TextInput>
+                <TextInput secureTextEntry={true} style={Style.textInput} onChangeText={(text) => this.setState({ confirmedPassword: text })}></TextInput>
                 <View style={{flexDirection: 'row'}}>
                     <TouchableOpacity style={[Style.button, {marginRight: 5}]} onPress={() => {this.props.navigation.goBack()}}><Text style={Style.buttonText}> Back</Text></TouchableOpacity>
                     <TouchableOpacity style={[Style.button, {marginLeft: 5}]} onPress={() => {this.Register()}}><Text style={Style.buttonText}> Register</Text></TouchableOpacity>
